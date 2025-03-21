142e78f7b37a3c1f8f2c7c00ed1d5a1809d43286
Heitor Oliveira <albuquerquedeoliveiraheitor@gmail.com>
2025-03-20 21:30:11 -0300

    commit

---
+++ urna/etapas.js
@@ -0,0 +1,50 @@
+let etapas = [
+    {
+        titulo: 'VEREADOR',
+        numeros: 5,
+        candidatos: [
+            {
+                numero: '22222',
+                nome: 'João Victor Família',
+                partido: 'PL',
+                fotos:[
+                    {url:'joaovictorfamilia.jpg', legenda: 'Vereador'}
+                ]
+            },
+            {
+                numero: '11888',
+                nome: 'Arthur Cozzolino',
+                partido: 'PP',
+                fotos:[
+                    {url:'arthurcozzolino.jpg', legenda: 'Vereador'}
+                ]
+            },
+        ]
+    },
+    {
+        titulo: 'PREFEITO',
+        numeros: 2,
+        candidatos: [
+            {
+                numero: '11',
+                nome: 'Renato Cozzolino',
+                partido: 'PP',
+                vice: 'Jamile Cozzolino',
+                fotos:[
+                    {url:'renato.jpg', legenda: 'Prefeito'},
+                    {url:'jamile.jpg', legenda: 'Vice-Prefeito', small: true}
+                ]
+            },
+            {
+                numero: '22',
+                nome: 'Ricardo Da Karol',
+                partido: 'PL',
+                vice: 'Léo Da Vila',
+                fotos:[
+                    {url:'ricardo.jpg', legenda: 'Prefeito'},
+                    {url:'leodavila.jpg', legenda: 'Vice-Prefeito', small: true}
+                ]
+            },
+        ]
+    }
+];
